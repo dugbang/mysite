4dc2c8713fb9df8de6a5af79fd2a133789f6cba9
dugbang <dugbang@gmail.com>
2018-08-24 23:25:09 +0900

    * postgresql db 로 변경함.

--- mysite/settings.py
+++ mysite/settings.py
@@ -122,12 +122,12 @@ DATABASES = {
         # 'ENGINE': 'django.db.backends.sqlite3',
         # 'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
 
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'dugbang_db',
-        'USER': 'dugbang',
-        'PASSWORD': 'dugbang1',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'mysite_db',
+        'USER': 'mysite_us',
+        'PASSWORD': 'mysite_pw',
         'HOST': 'localhost',
-        'PORT': '3306',
+        'PORT': '',
     }
 }
 
